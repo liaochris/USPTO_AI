97cf498838fc0f76b45e8e16941249fc9c6df5e6
Chris Liao <christopher.liao.2002@gmail.com>
2022-03-22 00:55:41 -0400

    updated data_filter.R

--- data_filter.R
+++ data_filter.R
@@ -25,12 +25,18 @@ setwd("~/Google Drive/Non-Academic Work/Research/Traina/Post-2022/USPTO_AI/")
 
 ai_data <- fread("data/ai_model_predictions.tsv")
 ai_data_filt <- ai_data[predict50_any_ai == 1]
-fwrite(ai_data_filt, "ai_model_predictions_filt.csv")
-
-ai_patent_app_ids <- ai_data_filt[flag_patent == 1, appl_id]
-length(ai_patent_app_ids)
 
 app_data <- fread("data/application.tsv")
+ai_patent_app_ids <- ai_data_filt[flag_patent == 1, appl_id]
 app_data_filt <- app_data[number %in% ai_patent_app_ids]
+
+patent_data <- fread("data/patent.tsv")
+location_data <- fread("data/location.tsv")
+inventor_data <- fread("data/inventor.tsv")
+assignee_data <- fread("data/assignee.tsv")
+
+
+fwrite(ai_data_filt, "ai_model_predictions_filt.csv")
+fwrite(app_data_filt, "ai_model_predictions_filt.csv")
 
 dim(app_data_filt)
